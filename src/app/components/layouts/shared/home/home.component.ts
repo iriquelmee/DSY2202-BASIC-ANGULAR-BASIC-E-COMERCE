f6efac52b662804ec1e9a0baf7a6fb87f826c483
iriquelmee <i.riquelmee@duocuc.cl>
2024-06-24 23:45:33 -0400

    fix en componente home se agregan productos de manera responsiva y se listan de manera apropiada pudiendo ser agregados al carrito.

--- src/app/components/layouts/shared/home/home.component.ts
+++ src/app/components/layouts/shared/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CartItem } from '../../../../models/cart-item.model';
+import { productos } from '../../../../data';
+import { CartService } from '../../../../services/cart.service';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +13,14 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   title: string = 'Home';
+  products = productos; // Assuming productos is your array of products
+  cartItems: CartItem[] = [];
+
+  constructor(private cartService: CartService) {}
+
+  addToCart(product: any){
+    this.cartService.addItem(product);
+    alert('¡Producto añadido al carrito con éxito!');
+  }
 
 }
