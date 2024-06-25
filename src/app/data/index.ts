c54f88f65b519a240dd33ad8562a8d16bca4a83a
iriquelmee <i.riquelmee@duocuc.cl>
2024-06-24 23:47:00 -0400

    se agrega modelo para items carrito y ademas se carga idex.ts con data preliminar que muestra data para navegacion, footer y diferentes compoenntes como el acordion y cards etc.

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -1,6 +1,69 @@
 export const nav = [
-    { name: 'home', href: '/' },
-    { name: 'games', href: '/' },
-    { name: 'about', href: '/' },
-    { name: 'faq', href: '/' },
-  ];
+  { name: 'Home', href: '/home' },
+  { name: 'About', href: '/about' },
+  { name: 'Faq', href: '/faq' },
+  { name: 'Cart', href: '/cart' },
+];
+
+export const accordion = {
+  title: 'Preguntas frecuentes',
+  subtitle:
+    'Todo lo que necesitas saber sobre Table Top',
+  faqs: [
+    {
+      id: 0,
+      que: "¿Cuáles son los medios de pago aceptados?",
+      res: "Los únicos requisitos son un computador con navegador web e internet, para poder acceder a la plataforma. Para los repartidores, deben usar Android 8.0 o superior.",
+    },
+    {
+      id: 1,
+      que: "¿Realmente puedo tener el control total?",
+      res: "Los únicos requisitos son un computador con navegador web e internet, para poder acceder a la plataforma. Para los repartidores, deben usar Android 8.0 o superior.",
+    },
+    {
+      id: 2,
+      que: "¿Qué pasa si un cliente ya no trabaja conmigo? ¿Puedo bloquearle el acceso?",
+      res: "Los únicos requisitos son un computador con navegador web e internet, para poder acceder a la plataforma. Para los repartidores, deben usar Android 8.0 o superior.",
+    },
+    {
+      id: 3,
+      que: "¿Qué requisitos necesito para poder usar BearTrack?",
+      res: "Los únicos requisitos son un computador con navegador web e internet, para poder acceder a la plataforma. Para los repartidores, deben usar Android 8.0 o superior.",
+    },
+    {
+      id: 4,
+      que: "¿Cómo es el cobro?",
+      res: "Los únicos requisitos son un computador con navegador web e internet, para poder acceder a la plataforma. Para los repartidores, deben usar Android 8.0 o superior.",
+    },
+  ],
+};
+
+export const productos = [
+  {
+    id: 1,
+    nombre: "Monopoly",
+    descripcion: "El clásico juego de compra y venta de propiedades.",
+    precio: 18000,
+    stock: 20,
+    categoria: "Juegos de Mesa",
+    img: 'assets/img/game-1.png'
+  },
+  {
+    id: 2,
+    nombre: "Uno",
+    descripcion: "Un emocionante juego de cartas donde debes deshacerte de todas tus cartas.",
+    precio: 9000,
+    stock: 25,
+    categoria: "Juegos de Cartas",
+    img: 'assets/img/game-2.png'
+  },
+  {
+    id: 3,
+    nombre: "Mesa de Ajedrez",
+    descripcion: "Una mesa de ajedrez de madera con tablero y piezas.",
+    precio: 15000,
+    stock: 10,
+    categoria: "Juegos de Mesa",
+    img: 'assets/img/game-3.png'
+  }
+];
---
+++ src/app/models/cart-item.model.ts
@@ -0,0 +1,9 @@
+export interface  CartItem {
+    id: number,
+    nombre: string,
+    descripcion: string
+    precio: number,
+    stock: number,
+    categoria: string,
+    img: string
+}
