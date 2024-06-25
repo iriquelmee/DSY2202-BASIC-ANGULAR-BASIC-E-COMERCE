9be184a67cd6d9560651c569a9f4496b241b7b6b
iriquelmee <i.riquelmee@duocuc.cl>
2024-06-24 21:51:03 -0400

    se importa componenntes footer y nav antes y despues de router outlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
 import { NavComponent } from './components/layouts/shared/nav/nav.component';
+import { FooterComponent } from './components/layouts/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, RouterLink, NavComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule, RouterLink, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
